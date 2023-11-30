bc0e9c11bccfa02219b3fef4cf06995de64fd046
Alejandro Ballesteros <aleballesterosh@gmail.com>
2023-11-30 17:32:59 -0600

    Comparison with simulations, binomial non-acummulative and original solution are added.

--- main.py
+++ main.py
@@ -71,12 +71,72 @@ start = time.time()
 #     resultados = db_conn.execute('SELECT * from results WHERE dice_sides=6 OR dice_sides=99 ORDER BY dice_sides ASC;').fetchall()
 #     print("<<<", resultados)
 
-# -------------------------------------- -------------------------------------- --------------------------------------
-stop = time.time()
-print(f"Elapsed time: {stop - start}.")
-
-
+# --- Binomial definition ---
 """
+Definition: For experiments that involves two possible results (success and failure), we can calculate the probability for 'x' succesful results 
+in the next 'n' executions, given a 'p' probability of success and a 'q' probability of failure, we cauculate that with the expression:
+        b(x; n, p) = (nCx)*((p**x)*(q**(n-x)))
+Where nCx is the total possible combinations in which the success event x can occur in the nex n executions, that can be calculated with factorials:
+        nCx = (n!)/(x!*(n-x)!)
+Where the ! symbol reffers the factorial function
+"""
+import math
+
+def truncated_factorial(start: int, stop: int):
+    fact = 1
+    for num in range(start, stop + 1):
+        fact *= num
+    return fact
+
+
+def combinations(n:int, x:int):
+    """For nCm -> n = n ; m = x"""
+    discard = n - x
+    top = truncated_factorial(discard+1, n)
+    bottom = math.factorial(x)
+    result = top/bottom
+    return int(result)
+
+def binomial_experiment(x:int, n: int, p: float):
+    """Non acummulative version for:
+    b(x; n, p) = (nCx)*( (p**x) * (q**(n-x)) )
+    where:
+    (nCx)      = combinations_part
+    (p**x)     = success_part
+    (q**(n-x)) = failure_part
+    """
+    q = 1 - p
+    combinations_part = combinations(n, x)
+    success_part = p**x
+    q_power = n - x
+    failure_part = q**q_power
+    result = combinations_part * success_part * failure_part
+    return result
+
+# ----- Comparison of experiment VS simulation results -----
+# For a 6 sided dice the probability that Bob wins is 1/6, thus the experiment for 1 success in the next executions will be:
+bobs_probability_1 = 1/6
+qty_success = 1
+qty_executions = 1
+# For a 99 sided dice the probability that Bob wins is 1/99:
+bobs_probability_2 = 1/99
+print("\n-------------------------------------- FOR A 6 SIDED DICE SCENARIO: --------------------------------------\n")
+print("Bob's Porbability according to binomial experiment: ", binomial_experiment(qty_success, qty_executions, bobs_probability_1))
+with db as db_conn:
+    resultados = db_conn.execute('SELECT dice_sides, rolls_qty, bob_wins, bobs_prob FROM results WHERE dice_sides=6;').fetchall()
+    print("Bob's results in the different simulations: \n", resultados)
+
+print("\n-------------------------------------- FOR A 99 SIDED DICE SCENARIO: --------------------------------------\n")
+print("Bob's Porbability according to binomial experiment: ", binomial_experiment(qty_success, qty_executions, bobs_probability_2))
+with db as db_conn:
+    resultados = db_conn.execute('SELECT dice_sides, rolls_qty, bob_wins, bobs_prob FROM results WHERE dice_sides=99;').fetchall()
+    print("Bob's results in the different simulations: \n", resultados)
+
+# -------------------------------------- -------------------------------------- --------------------------------------
+
+
+
+# #----- This section is keep because it was the original solution, it will have only one correction -----
 def bob_prob(min_sides: int, max_sides: int) -> list:
     # Bob's chance to win are 1/k given a k-sided dice, then for each dice we gather this chances in a list
     if max_sides < min_sides or min_sides < 6:
@@ -93,12 +153,14 @@ def bob_prob(min_sides: int, max_sides: int) -> list:
 
 def main(args):
     # Here the chances for Bob to win is 1/2 since he's playing with Alice, thus Bob's chances should be multiplied by 1/2
-    result = map(lambda x: x*0.5, bob_prob(6,99))
-    
-    return list(result)
+    # result = map(lambda x: x*0.5, bob_prob(6,99))
+    print("\n-------------------------------------- MAIN FUNCTION RESULTS FOR 6 AND 99 SIDED DICES: --------------------------------------\n")
+    results = bob_prob(6,99)  # <- This is the correction, no longer multiplying for 1/2
+    result = [results[0], results[-1]]
+    return result
 
 
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
-"""
+
--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,15 @@ class TestBobProb(unittest.TestCase):
     
     def test2(self):
         self.assertRaises(Exception, bob_prob, (6,2))
+        
+"""
+print("<<<<",combinations(52,13)) # Total bridge hands (13 cards) with regular deck of cards (52 cards)
+print(combinations(1,1))
+print(">>>", truncated_factorial(40,52)/math.factorial(13))  #truncated_factorial(1,13))
+print("<<<.>>>", binomial_experiment(1,1,(1/6)))
+print("<<<.>>>", binomial_experiment(2,5,0.1))
+print("<<<.>>>", binomial_experiment(3,5,0.5)) # Get exactly 3 heads in 5 coin toss
+"""
 
 if __name__== '__main__':
     unittest.main()
