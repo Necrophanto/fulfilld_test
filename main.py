ec28a324b35985f13c05446f379996fcb8ab3c04
Alejandro Ballesteros <aleballesterosh@gmail.com>
2023-11-30 15:09:43 -0600

    Sqlite DB file added.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from random import randint
+import time
+import sqlite3
 
 """
 Problem Statement
@@ -7,6 +10,98 @@ You have two players, Bob and Alice, that take turns in rolling a fair k-sided d
 k first wins the game. The Python program should output the probability that Bob wins the
 game for k = 6 thru 99. That is, the output will be an array of probabili8es where index 0 is the
 probability when k = 6; index 1 when k = 7; etc.
+"""
+
+
+
+db = sqlite3.connect('test.db')
+
+with db as conn:
+    cursor = conn.cursor()
+    # Create the table for the results
+    cursor.execute("""CREATE TABLE IF NOT EXISTS results (
+                dice_sides integer,
+                rolls_qty integer,
+                bob_wins integer,
+                alice_wins integer,
+                bobs_prob real
+                )""")
+    conn.commit()
+
+
+
+
+
+
+
+def bob_rolls(sides: int):
+    b_roll = randint(1,sides)
+    return b_roll
+
+def ali_rolls(sides: int):
+    a_roll = randint(1,sides)
+    return a_roll
+
+def simulate_rolls(dice_sides: int, total_rolls: int):
+    scores = {'Bob':0, 'Alice':0}
+    for i in range(1,total_rolls+1):
+        bob_w = bob_rolls(dice_sides)
+        ali_w = ali_rolls(dice_sides)
+        # print(i)
+        if bob_w == dice_sides:
+            scores["Bob"] += 1
+        if ali_w == dice_sides:
+            scores["Alice"] += 1
+    
+    return scores
+
+# def quicker_simulation(rolls: int, sides: int):
+#     b_wins = 0
+#     a_wins = 0
+#     for i in range(rolls):
+#         bob = bob_rolls(sides)
+#         if bob == sides:
+#             b_wins += 1
+#         ali = ali_rolls(sides)
+#         if ali == sides:
+#             a_wins += 1
+#         # print(">>>",bob)
+#     print(b_wins,"winned by Bob.", float(b_wins/events))
+#     print(a_wins,"winned by Alice.", float(a_wins/events))
+# quicker_simulation(events,dice_sides)
+    
+
+start = time.time()
+# ----------------------------------------------------------------------------
+# ToDo:
+# | Run for 100, 1000, 10000, 100000, 1000000 
+# | Build binomial definition 
+# | Add bobs_prob field to table and recreate DB [DONE]
+# | Add context manager for db connection [DONE]
+events = 100000
+# dice_sides = 10
+# results = simulate_rolls(dice_sides,events)
+# print(">>>>>", results)
+# print(results['Bob']," winned by Bob.", f"Probability: {float(results['Bob']/events)}")
+# print(results['Alice']," winned by Alice.", f"Probability: {float(results['Alice']/events)}")
+
+# with conn as db_conn:
+#     for s in range(6,100):
+#         results = simulate_rolls(s,events)
+#         # print(f"For {s} sided dice, Bob winned {results['Bob']} times.", f"Bob's probability of winning: {float(results['Bob']/events)}")
+#         insertion = db_conn.execute(f"""INSERT INTO results(dice_sides, rolls_qty, bob_wins, alice_wins) 
+#                                     VALUES ({s}, {events}, {results['Bob']}, {results['Alice']});""")
+
+# with conn as db_conn:
+#     test = db_conn.execute('DELETE FROM results;')
+#     resultados = db_conn.execute('SELECT * from results;').fetchall()
+#     print("<<<", resultados)
+
+# ----------------------------------------------------------------------------
+stop = time.time()
+print(f"Elapsed time: {stop - start}.")
+
+
 """
 def bob_prob(min_sides: int, max_sides: int) -> list:
     # Bob's chance to win are 1/k given a k-sided dice, then for each dice we gather this chances in a list
@@ -32,3 +127,4 @@ def main(args):
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
+"""
